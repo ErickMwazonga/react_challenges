0dc9a1a2cbea7c0f8a8d19ddac02209ab0358e17
Erick Mwazonga <erimwazo@amazon.com>
2023-04-27 23:23:31 +0100

    update home icons

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,11 @@
 import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { FcTodoList, FcBusinessman, FcRating } from "react-icons/fc";
+import { SiTaichigraphics } from "react-icons/si";
+import { BsFillChatLeftQuoteFill } from "react-icons/bs";
+import { MdPassword, MdDarkMode } from "react-icons/md";
+import { TbInputSearch } from "react-icons/tb";
+import { RiNodeTree } from "react-icons/ri";
 import './App.css';
 
 const Home = () => {
@@ -7,15 +13,42 @@ const Home = () => {
 		<Fragment>
 			<h3>The Challenger</h3>
 			<div className="home-page">
-				<Link to="/todo">Todo</Link>
-				<Link to="/random-user">Random User</Link>
-				<Link to="/dots">Dots</Link>
-				<Link to="/joker">Joker</Link>
-				<Link to="/rating">Rating</Link>
-				<Link to="/password">Password</Link>
-				<Link to="/autocomplete">Auto-Complete</Link>
-				<Link to="/toggle-theme">Toggle Theme</Link>
-				<Link to="/file-structure">File Structure</Link>
+				<Link to="/todo">
+					<span className='title'>Rating</span>
+					<FcTodoList className='icon'/>
+				</Link>
+				<Link to="/random-user">
+					<span className='title'>Random User</span>
+					<FcBusinessman className='icon'/>
+				</Link>
+				<Link to="/dots">
+					<span className='title'>Dots</span>
+					<SiTaichigraphics className='icon'/>
+				</Link>
+				<Link to="/joker">
+					<span className='title'>Dots</span>
+					<BsFillChatLeftQuoteFill className='icon'/>
+				</Link>
+				<Link to="/rating">
+					<span className='title'>Rating</span>
+					<FcRating className='icon'/>
+				</Link>
+				<Link to="/password">
+					<span className='title'>Password</span>
+					<MdPassword className='icon'/>
+				</Link>
+				<Link to="/autocomplete">
+					<span className='title'>Auto-Complete</span>
+					<TbInputSearch className='icon'/>
+				</Link>
+				<Link to="/toggle-theme">
+					<span className='title'>Toggle Theme</span>
+					<MdDarkMode className='icon'/>
+				</Link>
+				<Link to="/file-structure">
+					<span className='title'>File Structure</span>
+					<RiNodeTree className='icon'/>
+				</Link>
 			</div>
 		</Fragment>
 	);
